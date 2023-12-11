82241d5f60db19a0c1afcd6c666a5d9edf51fe1b
Joni Kamarainen <joni.kamarainen@tut.fi>
2023-12-11 13:16:34 +0200

    added support for file recovery

--- rsync_folders.sh
+++ rsync_folders.sh
@@ -1,52 +1,54 @@
+# Usage:
+#  $ source rsync_folders
+#  $ source rsync_folders back
+
+
 # Add each pair separater by a white space (SOURCE: no slashing end TARGET:Yes slashding end)
 os=`uname`
 
-if [ "$os" = "Darwin" ]; then
-    print $os
-    SOURCES=(/Users/kamarain/Documents
-	     /Users/kamarain/Pictures
-	     /Users/kamarain/Videos
-	     /Users/kamarain/Desktop)
-    #	 /home/kamarain/Work/sdu_dk
-    #	 /home/kamarain/Work/lut_fi)
+if [ "$os" = "Darwin" ]; then # MacOS
+    echo "MacOS system"
+    SOURCEROOT=/Users/kamarain/
 else
-    SOURCES=(/home/kamarain/Documents
-	     /home/kamarain/Pictures
-	     /home/kamarain/Videos
-	     /home/kamarain/Desktop)
-    #	 /home/kamarain/Work/sdu_dk
-    #	 /home/kamarain/Work/lut_fi)
+    echo "Linux system"
+    SOURCEROOT=/home/kamarain/
 fi;
 
-TARGETS=(linux-ssh.tuni.fi:laptop_backup/
-	 linux-ssh.tuni.fi:laptop_backup/
-	 linux-ssh.tuni.fi:laptop_backup/
-	 linux-ssh.tuni.fi:laptop_backup/)
-##TARGETS=(staff-linux.cc.tut.fi:laptop_backup/
-##	 staff-linux.cc.tut.fi:laptop_backup/
-##	 staff-linux.cc.tut.fi:laptop_backup/
-##	 staff-linux.cc.tut.fi:laptop_backup/)
-#	 staff-linux.cc.tut.fi:laptop_backup/Work/
-#	 staff-linux.cc.tut.fi:laptop_backup/Work/)
-DELETE_AT_TARGET=(1 1 1 1 0 0)
+SOURCES=(Documents
+	 Pictures
+	 Videos
+	 Desktop)
+    #	 Work/sdu_dk
+    #	 Work/lut_fi)
 
-i=0;
-for src in ${SOURCES[@]}
-do
-    echo "---"
-    echo "BACKUPING FILES";
-    echo "---"
-    echo ITEM: $i
-    echo SOURCE: ${SOURCES[$i]}
-    echo TARGET: ${TARGETS[$i]}
-    echo DELETE OLD:${DELETE_AT_TARGET[$i]}
-    if [ ${DELETE_AT_TARGET[$i]} -eq 1 ]
-    then
-	echo rsync -avu --modify-window=10 --delete ${SOURCES[$i]} ${TARGETS[$i]}
-	rsync -avu --modify-window=10 --delete ${SOURCES[$i]} ${TARGETS[$i]}
-    else
-	echo rsync -avu --modify-window=10 ${SOURCES[$i]} ${TARGETS[$i]}
-	rsync -avu --modify-window=10 ${SOURCES[$i]} ${TARGETS[$i]}
-   fi;
-    let "i=$i+1";
-done
+TARGETROOT=linux-ssh.tuni.fi:laptop_backup/
+
+DELETE_AT_TARGET=(0 0 0 0 0 0)
+
+if [[ $1 == "back" ]]
+then
+    echo "RESTORING FILES"
+    i=0;
+    for src in ${SOURCES[@]}
+    do
+	echo "EXECUTING" rsync -avu --modify-window=10 ${TARGETROOT}${SOURCES[$i]} ${SOURCEROOT}
+	rsync -avu --modify-window=10 ${TARGETROOT}${SOURCES[$i]} ${SOURCEROOT} 
+	let "i=$i+1";
+    done
+else
+    echo "STORING FILES TO REMOVE DIR";
+    i=0;
+    for src in ${SOURCES[@]}
+    do
+	if [ ${DELETE_AT_TARGET[$i]} -eq 1 ]
+	then
+	    echo "EXECUTING" rsync -avu --modify-window=10 --delete ${SOURCEROOT}${SOURCES[$i]} ${TARGETS[$i]}
+	    rsync -avu --modify-window=10 --delete ${SOURCEROOT}${SOURCES[$i]} ${TARGETS[$i]}
+	else
+	    echo "EXECUTING" rsync -avu --modify-window=10 ${SOURCEROOT}${SOURCES[$i]} ${TARGETS[$i]}
+	    rsync -avu --modify-window=10 ${SOURCEROOT}${SOURCES[$i]} ${TARGETS[$i]}
+	fi;
+	let "i=$i+1";
+    done
+fi;
+
